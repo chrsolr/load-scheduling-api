3deb5ae0a053e76c80df525a18d57f36135205f2
iwchrsolr <christian@innowatts.com>
2024-11-02 00:42:30 -0500

    Added get credentials by config id

--- Controllers/CredentialController.cs
+++ Controllers/CredentialController.cs
@@ -36,6 +36,30 @@ public class CredentialController : ControllerBase
         return Ok(credentials);
     }
 
+    [HttpGet("/v1/org/credentials/{configId}")]
+    public async Task<ActionResult<CredentialDTO>> GetByConfigId(
+        [FromRoute] Guid configId
+    )
+    {
+        HttpContext.Items.TryGetValue("DecodedJwtToken", out var jwtToken);
+
+        var decodedToken = jwtToken as DecodedJwtToken;
+        var org = decodedToken?.Organizations.FirstOrDefault();
+
+        if (decodedToken is null || org is null)
+        {
+            return Unauthorized();
+        }
+
+        var credentials = await _credentialService.GetByConfigId(configId);
+        if (credentials is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(credentials);
+    }
+
     [HttpPatch("/v1/org/credentials/{credentialId}/activate")]
     public async Task<ActionResult<dynamic>> Activate(Guid credentialId)
     {
